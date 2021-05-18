920dc7e8c1687a9ee60abdf70d13145168fef226
Sbitnev <sbitnev.vladimir1@gmail.com>
2021-05-18 18:56:11 +0300

    Замена на double

--- 1.cpp
+++ 1.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
-bool Array_print(int a[], size_t size);
+bool Array_print(double a[], size_t size);
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	const size_t size = 4;
-	int a[size] = { 1, 22, 33, 444 };
+	double a[size] = { 1.1, 22.2, 33.3, 444.4 };
 	cout << "Печать массива размером " << size << " элемента:" << endl;
 	Array_print(a, size);
     return 0;
 }
-bool Array_print(int a[], size_t size)
+bool Array_print(double a[], size_t size)
 {
 	for (size_t i = 0; i < size; i++)
 	{
